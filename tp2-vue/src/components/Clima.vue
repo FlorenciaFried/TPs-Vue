6ff1e5d54e5dfbd1b480528d873ba290dbd4dc13
FlorenciaFried <florencia.d.fried@gmail.com>
2020-07-05 18:13:50 -0300

    Llamada a la api realizada

--- tp2-vue/src/components/Clima.vue
+++ tp2-vue/src/components/Clima.vue
@@ -1,17 +1,51 @@
 <template>
   <div>
-    <h1>Hola desde generar</h1>
+    {{ callToApi('La Plata') }}
+    {{ callToApi('Mar del Plata') }}
+    <h1>{{ info }}</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Clima',
+  name: 'Clima',
 
-    mounted:{
-        
+  data: function () {
+    return {
+      infolp: '',
+      errorlp: '',
+      infomdq: '',
+      errormdq: '',
     }
+  },
 
+  methods: {
+    callToApi: async function (ciudad) {
+      await axios
+        .get("http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + ",Argentina&appid=9b147eb1e617afe522a5c074780d5379")
+        .then((response) => {
+          if (ciudad === 'La Plata') {
+            this.infolp = response.data.main
+          }
+          if (ciudad === 'Mar del Plata') {
+            this.infomdq = response.data.main
+
+          }
+        })
+        .catch((error) => {
+          if (ciudad === 'La Plata') {
+            console.log(error)
+            this.errorlp = error;
+          }
+          if (ciudad === 'Mar del Plata') {
+            console.log(error)
+            this.errormdq = error;
+          }
+        })
+    },
+  },
 }
 </script>
 
